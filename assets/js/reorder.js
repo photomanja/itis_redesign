3de61056f52164e33a91f3442eaba1b8e1fd8620
Tobias Oetiker <tobi@oetiker.ch>
2021-08-26 10:31:50 +0200

    use the real images

---
+++ assets/js/reorder.js
@@ -0,0 +1,31 @@
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    
+    const targetNode = document.getElementById('z43-newslist');
+    const reorder = () => {
+        let item = targetNode.querySelector('.z43-news-item'); // Get the first item
+        let parentRect = targetNode.getBoundingClientRect();
+        let lastTop;
+        let lastGap;
+        while (item) {
+            let itemRect = item.getBoundingClientRect();
+            if (itemRect.top > lastTop
+                && lastGap > 0){
+                let iitem = item.nextElementSibling;
+                while (iitem) {
+                    let iitemRect = iitem.getBoundingClientRect();
+                    if (iitemRect.width < itemRect.width){
+                        item.before(iitem);
+                        break;
+                    }
+                    iitem = iitem.nextElementSibling;
+                }
+            }
+            lastTop = itemRect.top;
+            lastGap = parentRect.right - itemRect.right;
+            item = item.nextElementSibling;
+        }
+    };
+    reorder();
+    window.addEventListener('resize', reorder);
+});
